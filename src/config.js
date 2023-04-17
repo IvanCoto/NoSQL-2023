038af4054cd6cf0e39d7987ac54fd0916845904c
ivan coto jimenez <ivancototkd@gmail.com>
2023-04-16 19:51:18 -0600

    Cambio en el las variables del entorno

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ import { config } from "dotenv";
 config();
 
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb+srv://ivancototkd:HQB92qPim6ARO6gh@clusterninjadev.cb6dw1c.mongodb.net";
+  process.env.MONGODB_URI || "mongodb+srv://<username>:<password>@clusterninjadev.cb6dw1c.mongodb.net";
